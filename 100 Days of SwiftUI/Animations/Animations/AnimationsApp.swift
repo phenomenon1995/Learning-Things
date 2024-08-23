acec6aeaa6b59d7740a1aa0eaaea03682abba038
David Williams <109742173+phenomenon1995@users.noreply.github.com>
2024-08-23 11:58:59 -0400

    Completed Word Scramble Project 5, Staring on Animations

---
+++ 100 Days of SwiftUI/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by David Williams on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
