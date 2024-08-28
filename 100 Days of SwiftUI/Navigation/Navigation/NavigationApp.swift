86fcb6d100968b1ac925716fa3ecc9250bde8ee8
phenomenon1995 <phenomenon1995@gmail.com>
2024-08-28 10:40:41 -0400

    Created Navigation Technique Project

---
+++ 100 Days of SwiftUI/Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by David Williams on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
