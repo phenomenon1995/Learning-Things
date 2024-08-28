e835aeeefbd334ce8e58cc8e155676055b30afa6
phenomenon1995 <phenomenon1995@gmail.com>
2024-08-28 09:40:52 -0400

    Moonshot Challenge

--- 100 Days of SwiftUI/Moonshot/Moonshot/ContentView.swift
+++ 100 Days of SwiftUI/Moonshot/Moonshot/ContentView.swift
@@ -6,15 +6,66 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State private var isGrid: Bool = false
+
+    var body: some View {
+        NavigationStack{
+            Group{
+                if isGrid { MoonshotGridView() } else { MoonshotListView() }
+            }
+                .navigationTitle("Moonshot")
+                .background(.darkBackground)
+                .preferredColorScheme(.dark)
+                .toolbar{
+                    Button{
+                        isGrid.toggle()
+                    } label: {
+                        Text(isGrid ? "List View" : "Grid View")
+                            .font(.title3)
+                            .bold()
+                }
+            }
+        }
+    }
+}
+struct MoonshotListView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
-    
+    var body: some View {
+        NavigationStack{
+            List{
+                ForEach(missions){mission in
+                    NavigationLink{
+                        MissionView(mission: mission, astronauts: astronauts)
+                    } label: {
+                        HStack(spacing: 30){
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame( width: 100, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                            
+                            VStack(alignment: .leading){
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                Text(mission.formattedLaunchDate)
+                                    .font(.caption)
+                            }
+                        }
+                    }
+                }
+                .listRowBackground(Color.darkBackground)
+            }
+            .listStyle(.plain)
+        }
+    }
+}
+struct MoonshotGridView: View {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
     let columns = [
         GridItem(.adaptive(minimum: 150))
     ]
-    
     var body: some View {
         NavigationStack{
             ScrollView{
@@ -29,7 +80,7 @@ struct ContentView: View {
                                     .scaledToFit()
                                     .frame(width: 100, height: 100)
                                     .padding()
-                            
+                                
                                 VStack{
                                     Text(mission.displayName)
                                         .font(.headline)
@@ -53,9 +104,6 @@ struct ContentView: View {
                 }
                 .padding([.horizontal, .bottom])
             }
-            .navigationTitle("Moonshot")
-            .background(.darkBackground)
-            .preferredColorScheme(.dark)
         }
     }
 }
--- 100 Days of SwiftUI/Moonshot/Moonshot/MissionView.swift
+++ 100 Days of SwiftUI/Moonshot/Moonshot/MissionView.swift
@@ -15,78 +15,50 @@ struct CustomDivider: View {
     }
 }
 
-struct MissionView: View {
-    struct CrewMember {
-        let role: String
-        let astronaut: Astronaut
-    }
-    
-    let mission: Mission
-    let crew: Array<CrewMember>
-    
+struct CrewMember {
+    let role: String
+    let astronaut: Astronaut
+}
+
+struct CrewView: View {
+    var crew: Array<CrewMember>
     var body: some View {
-        ScrollView{
-            VStack{
-                Image(mission.image)
-                    .resizable()
-                    .scaledToFit()
-                    .containerRelativeFrame(.horizontal){width, axis in
-                        width * 0.6
-                    }
-                
-                
-                VStack(alignment: .leading){
-                    CustomDivider()
-                    Text("Mission Highlights")
-                        .font(.title.bold())
-                        .padding(.bottom, 5)
-                    Text(mission.description)
-                    CustomDivider()
-                    
-                    Text("Crew")
-                        .font(.title.bold())
-                        .padding(.bottom, 5)
-                }
-                .padding(.horizontal)
-                
-                ScrollView(.horizontal, showsIndicators: false){
-                    
-                    HStack{
-                        ForEach(crew, id: \.role){crewMember in
-                            NavigationLink {
-                                AstronautView(astronaut: crewMember.astronaut)
-                            } label: {
-                                HStack{
-                                    Image(crewMember.astronaut.id)
-                                        .resizable()
-                                        .frame(width:104, height: 72)
-                                        .clipShape(.capsule)
-                                        .overlay(
-                                            Capsule()
-                                                .strokeBorder(lineWidth: 1)
-                                        )
-                                    VStack(alignment: .leading){
-                                        Text(crewMember.astronaut.name)
-                                            .foregroundStyle(.white)
-                                            .font(.headline)
-                                        
-                                        Text(crewMember.role)
-                                            .foregroundStyle(.white.opacity(0.5))
-                                    }
-                                }
-                                .padding(.horizontal)
+        ScrollView(.horizontal, showsIndicators: false){
+            
+            HStack{
+                ForEach(crew, id: \.role){crewMember in
+                    NavigationLink {
+                        AstronautView(astronaut: crewMember.astronaut)
+                    } label: {
+                        HStack{
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width:104, height: 72)
+                                .clipShape(.capsule)
+                                .overlay(
+                                    Capsule()
+                                        .strokeBorder(lineWidth: 1)
+                                )
+                            VStack(alignment: .leading){
+                                Text(crewMember.astronaut.name)
+                                    .foregroundStyle(.white)
+                                    .font(.headline)
+                                
+                                Text(crewMember.role)
+                                    .foregroundStyle(.white.opacity(0.5))
                             }
                         }
+                        .padding(.horizontal)
                     }
                 }
             }
-            .padding(.bottom)
         }
-        .navigationTitle(mission.displayName)
-        .navigationBarTitleDisplayMode(.inline)
-        .background(.darkBackground)
     }
-    
+}
+
+struct MissionView: View {
+    let mission: Mission
+    let crew: Array<CrewMember>
     init(mission: Mission, astronauts: [String: Astronaut]){
         self.mission = mission
         self.crew = mission.crew.map { member in
@@ -97,12 +69,42 @@ struct MissionView: View {
             }
         }
     }
-    
+    var body: some View {
+        ScrollView{
+            VStack{
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal){width, axis in
+                        width * 0.6
+                    }
+                Text(mission.launchDate?.formatted(date: .complete, time: .omitted) ?? "Didn't Launch")
+                    .font(.subheadline)
+                VStack(alignment: .leading){
+                    CustomDivider()
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    Text(mission.description)
+                    CustomDivider()
+                    Text("Crew")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                }
+                .padding(.horizontal)
+                CrewView(crew: crew)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
 }
 
 #Preview {
     let missions: [Mission] = Bundle.main.decode("missions.json")
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-    return MissionView(mission: missions[0], astronauts: astronauts )
+    return MissionView(mission: missions[1], astronauts: astronauts )
         .preferredColorScheme(.dark)
 }
