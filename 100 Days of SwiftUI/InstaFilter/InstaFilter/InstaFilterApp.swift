b5b802b920b2299123a849371e15ee5b943520c9
phenomenon1995 <phenomenon1995@gmail.com>
2024-09-10 10:52:18 -0400

    Started Instafilter

---
+++ 100 Days of SwiftUI/InstaFilter/InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by David Williams on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
